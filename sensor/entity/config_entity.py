36bcc591c83c31179d3cb53fee1d225606b307e3
pranav1427 <pranavvanage@gmail.com>
2023-04-02 09:33:35 +0530

    update train test split

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -27,7 +27,7 @@ class DataIngestionConfig:
             self.database_name="aps"
             self.collection_name="sensor"
             self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir ,"data_ingestion")
-            self.feature_store_dir = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
+            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
             self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
             self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
             self.test_size = 0.2
